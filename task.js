a43864107ac506b8bc1b2d1eec9bbc970624199f
Mujahid <mansurimujahid@gmail.com>
2019-05-28 16:43:23 +0530

    updated functions

--- task.js
+++ task.js
@@ -1,12 +1,34 @@
-var a=10;
-var b=20;
-
-    c = a+b;
-    console.log('Addition of the ' + a +' nd '+ b +' is ' + c);
-    c = a-b;
-    console.log('Subtraction of the ' + a +' nd '+ b +' is ' + c);;
-    c = a*b;
-    console.log('Multiplication of the ' + a +' nd '+ b +' is ' + c);
-    c = a/b;
-
-    console.log('Devide of the ' + a +' nd '+ b +' is ' + c);
+    var a = 10;
+    var b = 20;
+    
+    // function
+    function addition(a, b) 
+    {
+        return a + b;
+    }
+    console.log('Addition of ' + a +' and '+ b + ' = ' + addition(a,b));
+    
+    function subtraction(a, b) 
+    {
+        return a - b;
+    }
+    console.log('Subtraction of ' + a +' and '+ b + ' = ' + subtraction(a,b));
+    
+    function multiplication(a, b) 
+    {
+        return a * b;
+    }
+    console.log('Multiplication of ' + a +' and '+ b + ' = ' + multiplication(a,b));
+    
+    function division(a, b) 
+    {
+        return a / b;
+    }
+    console.log('Division of ' + a +' and '+ b + ' = ' + division(a,b));
+    
+    function module(a, b) 
+    {
+        return a % b;
+    }
+    console.log('Module of ' + a +' and '+ b + ' = ' + module(a,b));
+    
