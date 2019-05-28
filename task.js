fad16eef808eb70df8563f847945baf6ebe5b149
Mujahid <mansurimujahid@gmail.com>
2019-05-28 15:43:17 +0530

    first task

---
+++ task.js
@@ -0,0 +1,12 @@
+var a=10;
+var b=20;
+
+    c = a+b;
+    console.log('Addition of the ' + a +' nd '+ b +' is ' + c);
+    c = a-b;
+    console.log('Subtraction of the ' + a +' nd '+ b +' is ' + c);;
+    c = a*b;
+    console.log('Multiplication of the ' + a +' nd '+ b +' is ' + c);
+    c = a/b;
+
+    console.log('Devide of the ' + a +' nd '+ b +' is ' + c);
